cd8f5044fb7f58996e5fe270ccc4e9ef237e592d
Daniel Barry <dlbarry@gmail.com>
2020-10-20 21:41:44 +1100

    inital commit

---
+++ Source/lovecrafting/lovecraftingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "lovecraftingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOVECRAFTING_API AlovecraftingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
